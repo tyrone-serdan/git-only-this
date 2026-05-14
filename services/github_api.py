5843f1e01e7afe29d54f296a86826c5e3d949b1c
Tyrone Serdan <tyroneb.serdan@gmail.com>
2026-05-14 17:05:11 +0800

    chore: fixed github_api indentation inconsistencies

--- services/github_api.py
+++ services/github_api.py
@@ -3,36 +3,36 @@ from github import Github
 gh = Github()
 
 def parse_url(url: str) -> str:
-	# TODO: actually make the implementation
-	# ______________________________________
-	# Possible inputs by a user:
-	# 	https://github.com/user/repository
-	#		https://github.com/tyrone-serdan/minimal-blog-astro/
-	# 	https://github.com/tyrone-serdan/minimal-blog-astro.git
-	#		git@github.com:tyrone-serdan/minimal-blog-astro.git
-	# _________________________________________________________
-	#	the package im using will only accept:
-	#		tyrone-serdan/minimal-blog-astro
-	parsed: str = url
+    # TODO: actually make the implementation
+    # ______________________________________
+    # Possible inputs by a user:
+    #     https://github.com/user/repository
+    #     https://github.com/tyrone-serdan/minimal-blog-astro/
+    #     https://github.com/tyrone-serdan/minimal-blog-astro.git
+    #     git@github.com:tyrone-serdan/minimal-blog-astro.git
+    # _________________________________________________________
+    #    the package im using will only accept:
+    #        tyrone-serdan/minimal-blog-astro
+    parsed: str = url
 
-	return parsed
+    return parsed
 
 
 
 def get_repo_filepaths(repo_link: str) -> list[str]:
-	files: list[str] = []
-	repo = gh.get_repo(parse_url(repo_link))
+    files: list[str] = []
+    repo = gh.get_repo(parse_url(repo_link))
 
-	branch = repo.get_branch(repo.default_branch)
+    branch = repo.get_branch(repo.default_branch)
 
-	# TODO: make user select which commit/branch to download from (?)
-	tree = repo.get_git_tree(
-		branch.commit.sha,
-		recursive=True
-	)
+    # TODO: make user select which commit/branch to download from (?)
+    tree = repo.get_git_tree(
+        branch.commit.sha,
+        recursive=True
+    )
 
-	for item in tree.tree:
-		if item.type == "blob":
-			files.append(item.path)
+    for item in tree.tree:
+        if item.type == "blob":
+            files.append(item.path)
 
-	return files
+    return files
